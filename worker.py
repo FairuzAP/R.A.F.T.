4df2c5bdf539374ec56b817656fe63dd9bff390b
13514104 <13514104@std.stei.itb.ac.id>
2017-04-18 19:01:31 +0700

    Fixed worker port input
    Signed-off-by: 13514104 <13514104@std.stei.itb.ac.id>

--- worker.py
+++ worker.py
@@ -15,6 +15,7 @@ workerhost = []
 balancerhost = []
 daemon_delay = 0.5
 pid = getpid()
+id = 1
 
 
 # The main worker server class
@@ -70,26 +71,45 @@ def load_conf():
 
     # Close the config file
     conf.close()
-    print("Config file loaded")
+    print("Config file loaded successfully")
+
+
+# Get the desired port and the server ID from the conf
+def get_port():
+    global id
+    print("There seems to be " +workerhost.__len__().__str__()+ " worker hosts in the system:")
+    for host in workerhost:
+        print(id.__str__() +". "+ host.__str__())
+        id += 1
+
+    try:
+        id = int(input("Which one am I supposed to be? "))
+        start = workerhost[id-1].find(":", 8)
+        end = workerhost[id-1].find("/", start)
+        return int(workerhost[id-1][start+1:end])
+    except:
+        print("Error in parsing port number")
+        sys.exit()
 
 
 # Get the current main thread workload
 def get_workload():
-    return psutil.Process(pid).cpu_percent(interval=1.0)
+    return psutil.Process(pid).cpu_percent(interval=daemon_delay)
 
 
 # Periodically broadcast workload to all balancerhost
 def worker_daemon_method():
     while(True):
         current_workload = get_workload().__str__()
-        print("Current workload is " + current_workload)
+        print("Broadcasting current workload of " + current_workload)
 
         for url in balancerhost:
             try:
-                r = requests.get(url + "load/" + current_workload, timeout=0.001)
+                requests.get(url + "load/" + current_workload, timeout=0.001)
             except Exception as e:
                 print("Workload broadcast failed for " + url)
                 pass
+
         sleep(daemon_delay)
 
 
@@ -97,8 +117,13 @@ def main():
     load_conf()
 
     # Get the desired port and prepare the server
-    current_port = int(input("Which port will this worker run at?\n"))
-    worker = HTTPServer(("", current_port), WorkerHandler)
+    current_port = get_port()
+    try:
+        worker = HTTPServer(("", current_port), WorkerHandler)
+    except:
+        print("Error in starting worker server")
+        sys.exit()
+    print("Worker Server " +id.__str__()+ " Running at port " + current_port.__str__())
 
     # Start the server daemon and the server
     worker_daemon = threading.Thread(target=worker_daemon_method)
